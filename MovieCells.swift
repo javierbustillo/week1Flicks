9f5ddab46a85c1335cdf15ddc37326322bd5154d
javierbustillo <Javdav@Javiers-MacBook-Pro.local>
2016-01-23 13:32:05 -0400

    Monday

---
+++ MovieCells.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCells.swift
+//  Flicks
+//
+//  Created by Javier Bustillo on 1/23/16.
+//  Copyright © 2016 Javier Bustillo. All rights reserved.
+//
+
+import UIKit
+
+class MovieCells: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+
+    @IBOutlet weak var overviewLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
